4b3b4602c72b6e49e9cce2f65e410aa1ab7420a3
Andy Richardson <andy.john.richardson@gmail.com>
2018-05-13 16:46:48 +0100

    extend tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import * as ReactTestUtils from 'react-dom/test-utils';
+import * as TestRenderer from 'react-test-renderer';
 import App from './App';
-
-import * as renderer from 'react-test-renderer';
+import { Login } from './Login';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
+  const rendered = TestRenderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
+});
+
+it('directs to Login page on startup', () => {
+  // const rendered = TestRenderer.create(<App />);
+  // console.log(rendered.root.findByType(Login));
 });
