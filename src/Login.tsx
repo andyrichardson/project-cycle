27131e57bfe9e6cd25c88f421922443a91724318
Andy Richardson <andy.john.richardson@gmail.com>
2018-05-13 13:20:36 +0100

    add logo and register button

--- src/Login.tsx
+++ src/Login.tsx
@@ -20,6 +20,8 @@ export class Login extends React.Component<Props, any> {
   public render() {
     return (
       <PageView>
+        <Logo source={require('./assets/images/logo-white.png')} />
+        <BrandText>Cycle Mate</BrandText>
         <LoginInput
           placeholder="Email address"
           onChangeText={text => this.props.authStore.setEmail(text)}
@@ -33,16 +35,34 @@ export class Login extends React.Component<Props, any> {
         <LoginButton onPress={() => this.props.authStore.login()}>
           <ButtonText>Login</ButtonText>
         </LoginButton>
+
+        <RegisterButton onPress={() => this.props.authStore.login()}>
+          <ButtonText>Register</ButtonText>
+        </RegisterButton>
       </PageView>
     );
   }
 }
 
 const PageView = styled.View`
+  align-items: center;
   background: #f00;
   flex: 1;
   justify-content: center;
   padding: 10px;
+`;
+
+const Logo = styled.Image`
+  height: 100px;
+  width: 100px;
+`;
+
+const BrandText = styled.Text`
+  color: #fff;
+  font-size: 25px;
+  font-family: 'Lato';
+  margin-bottom: 50px;
+  margin-top: 20px;
 `;
 
 const LoginInput = styled.TextInput`
@@ -55,6 +75,14 @@ const LoginInput = styled.TextInput`
 const LoginButton = styled.TouchableHighlight`
   align-items: center;
   background-color: #c00;
+  margin-top: 30px;
+  padding: 15px;
+  width: 100%;
+`;
+
+const RegisterButton = styled.TouchableHighlight`
+  align-items: center;
+  margin-top: 5px;
   padding: 15px;
   width: 100%;
 `;
