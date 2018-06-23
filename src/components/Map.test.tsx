23657784e98d89d8a2fa83631225aa820d3cdcc0
Andy Richardson <andy.john.richardson@gmail.com>
2018-06-23 20:06:49 +0100

    add markers test

--- src/components/Map.test.tsx
+++ src/components/Map.test.tsx
@@ -4,6 +4,9 @@ import Permissions from 'react-native-permissions';
 import * as TestRenderer from 'react-test-renderer';
 import { MapComponent } from './Map';
 jest.mock('react-native-permissions');
+jest.mock('react-native-percentage-circle', () => ({
+  default: 'react-native-percentage'
+}));
 
 describe('initialize', () => {
   let props: MapComponentProps;
@@ -95,4 +98,54 @@ describe('location', () => {
   //   const calls = navigator.geolocation.getCurrentPosition.mock.calls;
   //   expect(calls.length).to.equal(1);
   // });
+});
+
+describe('markers', () => {
+  let props: MapComponentProps;
+
+  beforeEach(() => {
+    // @ts-ignore
+    const points: PointState = {
+      error: false,
+      fetching: false,
+      lastUpdated: null,
+      results: [
+        {
+          bikes: { available: 4, total: 4 },
+          id: 1,
+          location: { latitude: 51.4, longitude: -0.10997 },
+          name: 'Bike test 1'
+        },
+        {
+          bikes: { available: 10, total: 3 },
+          id: 2,
+          location: { latitude: 51.529163, longitude: -0.10997 },
+          name: 'Bike test 2'
+        },
+        {
+          bikes: { available: 9, total: 6 },
+          id: 3,
+          location: { latitude: 51.529163, longitude: -0.11997 },
+          name: 'Bike test 3'
+        }
+      ]
+    };
+
+    props = {
+      points,
+      updatePoints: jest.fn()
+    };
+  });
+
+  it('renders with points', () => {
+    TestRenderer.create(<MapComponent {...props} />).toJSON();
+  });
+
+  it('renderMarker returns marker', () => {
+    const instance = new MapComponent(props);
+
+    // @ts-ignore
+    const marker = instance.renderMarker(props.points.results[0]);
+    expect(marker.type).to.equal('marker');
+  });
 });
