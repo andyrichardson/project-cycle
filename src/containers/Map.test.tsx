7c926da44671dc51f4a8327bf9282b80e84fb339
Andy Richardson <andy.john.richardson@gmail.com>
2018-06-23 16:39:31 +0100

    add container test

---
+++ src/containers/Map.test.tsx
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+import * as React from 'react';
+import * as TestRenderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import { Map } from './Map';
+jest.mock('../components');
+
+describe('initialize', () => {
+  const middlewares = [];
+  const mockStore = configureStore(middlewares);
+  let store: any;
+
+  beforeEach(() => {
+    store = mockStore({
+      points: []
+    });
+  });
+
+  it('constructs', () => {
+    const map = new Map({ store });
+
+    expect(map).to.be.an('object');
+  });
+
+  it('renders', () => {
+    const rendered = TestRenderer.create(
+      // @ts-ignore
+      <Map store={store} />
+    ).toJSON();
+  });
+});
