287f66e0385efb22a33642d287f9e7c789f42277
Andy Richardson <andy.john.richardson@gmail.com>
2018-07-15 23:16:53 +0100

    add jest babel dependency

--- config/setup.ts
+++ config/setup.ts
@@ -1,8 +1,5 @@
 // tslint:disable
 // @ts-ignore
-global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
-// @ts-ignore
 global.fetch = jest.fn().mockResolvedValue(true);
 
 const mockGeolocation = {
